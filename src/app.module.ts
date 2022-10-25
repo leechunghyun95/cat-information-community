d3f0603f79a1b71101e8fcb0303437392bbd6717
Neo <neo@kiwistud.io>
2022-10-25 22:26:25 +0900

    Feat: DB 연동

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,17 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
 import { LoggerMiddleware } from './middlewares/logger.middleware';
 
 @Module({
-  imports: [CatsModule],
+  imports: [
+    CatsModule,
+    MongooseModule.forRoot(
+      'mongodb+srv://neo:FUX4mtSOETisgW9u@nestcluster.s3yxd4h.mongodb.net/cats?retryWrites=true&w=majority',
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
