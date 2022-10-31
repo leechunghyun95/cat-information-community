afc932eab2079230d9ae96f71e3097f0cc80dc8d
Neo <neo@kiwistud.io>
2022-11-01 00:00:24 +0900

    Feat: DB 스키마, Controller 설계 & validation

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,42 +1,22 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-} from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 import { CatsService } from './cats.service';
-import { CreateCatDto } from './dto/create-cat.dto';
-import { UpdateCatDto } from './dto/update-cat.dto';
 
 @Controller('cats')
 export class CatsController {
   constructor(private readonly catsService: CatsService) {}
 
-  @Post()
-  create(@Body() createCatDto: CreateCatDto) {
-    return this.catsService.create(createCatDto);
-  }
-
   @Get()
-  findAll() {
-    return this.catsService.findAll();
-  }
+  getCurrentCat() {}
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.catsService.findOne(+id);
-  }
+  @Post()
+  signUp() {}
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateCatDto: UpdateCatDto) {
-    return this.catsService.update(+id, updateCatDto);
-  }
+  @Post('login')
+  logIn() {}
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.catsService.remove(+id);
-  }
+  @Post('logOut')
+  logOut() {}
+
+  @Post('upload/cats')
+  uploadCatImg() {}
 }
